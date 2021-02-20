f862a94fca6a0ddc77824b3a1f5f18506ad9781d
dania-c <workingdania@hotmail.com>
2021-02-20 12:18:55 +0200

    add this in command prompt to watch the data 'npx json-server --watch data/db.json --port 8000'

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,27 @@ import {useState,useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
    // let name = 'marim';
-    const [blogs,setBlogs] = useState([
-        {title:'web 1', body:'this is the body of web1 ...',author:'author1',id: 1},
-        {title:'web 2', body:'this is the body of web2 ...',author:'author2',id: 2},
-        {title:'web 3', body:'this is the body of web3 ...',author:'author3',id: 3}
-    ]);
+   
+   const [blogs,setBlogs] = useState(null);
   const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !==id);
       setBlogs(newBlogs);
   }
 
-   useEffect(()=>{console.log('use Efect running')});
+   useEffect(()=>{
+    fetch('http://localhost:8000/blogs')
+    .then(res=> {
+        return res.json();
+    })
+    .then(data => {
+        setBlogs(data);
+      })
+  }, []);
 
-       
+          
     return ( 
         <div className="home">
-            <BlogList blogs= {blogs} title="Allblogs!" handleDelete={handleDelete}/>
+            { blogs && <BlogList blogs= {blogs} title="Allblogs!" handleDelete={handleDelete}/>}
             
             
         </div>
